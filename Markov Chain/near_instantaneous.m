4969e0b394345f73ca65d9d2a7fe4d5f174345da
David E. Acuña-Ureta <52395340+david-acuna@users.noreply.github.com>
2022-09-12 00:00:16 -0300

    Add files via upload

---
+++ Markov Chain/near_instantaneous.m
@@ -0,0 +1,140 @@
+%% Authorship
+% David E. Acuña-Ureta, Ph.D.
+% Assistant Professor
+% Department of Mechanical and Metallurgical Engineering
+% Pontificia Universidad Católica de Chile
+% E-mail: david.acuna@ing.puc.cl
+
+%% Definitions
+% Table 1: Simulation parameters
+Ts = 1.0;           % sample time
+Ecrit = 1389900;    % battery storage capacity
+sigma_w = 1e-6;     % standard deviation process noise
+sigma_eta = 0.9;    % (set 0.9 or 0.1) standard deviation measurement noise
+v0 = 41.405;        % open-circuit voltage model parameter
+vL = 33.481;        % open-circuit voltage model parameter
+alpha = 5.319e-3;   % open-circuit voltage model parameter
+beta = 11.505;      % open-circuit voltage model parameter
+gamma = 1.5538;     % open-circuit voltage model parameter
+R = 0.26;           % internal resistance
+
+% state-space functions
+Voc = @(x) vL + (v0-vL)*exp((x-1)*gamma) + alpha*vL*(x-1) + ...
+    (1-alpha)*vL*(exp(-beta)-exp(-beta*sqrt(x)));               % Eq. (3): open-circuit voltage model
+V = @(x,u) Voc(x) - R*u + randn*sigma_eta;                      % Eq. (2): measurement equation
+state_trans = @(x,u) x - u*V(x,u)*(Ts/Ecrit) + randn*sigma_w;   % Eq. (1): state transition equation
+
+% Exogenous inputs (current discharge) modeled as a two-state Markov Chain
+T = [0.9780 0.0255; 0.0220 0.9745];     % Eq. (37): state transition kernel Markov Chain
+[EigVec,~] = eig(T);                    
+Ps = EigVec(:,1)./sum(EigVec(:,1));     % Eq. (38): stationary distribution
+s = [9.7024; 8.3991];                   % Eqs. (35)-(36): states of the Markov Chain
+
+% Threshold
+v_cutoff = 33;  % cut-off voltage
+
+% Uncertain event likelihood function (a.k.a. "uncertain hazard zone")
+PEv = @(v,u) 1 - (1/2)*(1+erf(( v - (v_cutoff+R*u) )/(sqrt(2)*sigma_eta))); % Eq. (15): uncertain event likelihood function
+
+% Other simulation parameters
+kp = 800;   % time at which prognostics begin
+kh = 3400;  % time at which prognostics finish
+x0 = 0.8;   % State-of-Charge at time 'kp'
+
+%% Method 1 (Standard): Certain event approach
+N = 1e3;                                                % number of Monte Carlo simulations
+histogram_bins = zeros(1,kh-kp+1);                      % count of frequency at each histogram bin
+start_time = cputime;                                   % starting time
+for traj=1:N                                            % simulation of trajectories
+    X = x0;                                             % set initial state
+    U = s(randsrc(1,1,[(1:length(s));Ps']));            % set initial exogenous input
+    for k=2:kh-kp+1
+        X = state_trans(X,U);                           % sample x_{k+1} ~ p(x_{k+1}|x_k,u_k)
+        U = s(randsrc(1,1,[(1:length(s));Ps']));        % sample u_{k+1} ~ p(u_{k+1})
+        if (V(X,U)<=v_cutoff)                           % if cut-off voltage threshold is hit
+            histogram_bins(k) = histogram_bins(k) + 1;	% sum contribution to histogram
+            break;                                      % stop simulating this trajectory
+        end
+    end
+end
+computation_time = cputime - start_time;                % Eq. (34): computation time
+if sigma_eta == 0.9                                     % load ground truth probability distribution
+    load('ground_truth_eta09.mat');	
+elseif sigma_eta == 0.1
+    load('ground_truth_eta01.mat');
+end
+EoD_time_PMF = histogram_bins./sum(histogram_bins);    	% Eq. (22): EoD time probability distribution
+l1_distance = norm(EoD_ground_truth-EoD_time_PMF,1);    % Eq. (33): l1-distance to ground truth
+
+% plot results
+figure
+hold on
+h1 = area(kp:kh,EoD_ground_truth);
+h1.FaceColor = [0.6350, 0.0780, 0.1840];
+h1.FaceAlpha = 0.4;
+h1.EdgeColor = [0.6350, 0.0780, 0.1840];
+h2 = area(kp:kh,EoD_time_PMF);
+h2.FaceColor = [0, 0.4470, 0.7410];
+h2.FaceAlpha = 0.4;
+h2.EdgeColor = [0, 0.4470, 0.7410];
+xlabel('Time (s)')
+ylabel('Probability')
+legend('Ground truth',sprintf('Method 1: N=%d',N),'location','northwest')
+if sigma_eta == 0.9
+    axis([800 2200 0 3e-3])
+elseif sigma_eta == 0.1
+    axis([3000 3350 0 0.015])
+end
+fprintf('l1-distance (efficacy): %f\n',l1_distance)                       % display efficacy
+fprintf('Computation time (efficiency): %f seconds\n',computation_time)   % display efficiency
+
+%% Method 2 (Proposed): Uncertain event approach
+N = 1;                                              % number of Monte Carlo simulations
+histogram_bins = zeros(1,kh-kp+1);                	% count of frequency at each histogram bin
+start_time = cputime;                               % starting time
+rho_min = 1e-3;                                     % parameter criteria for stopping simulations
+for traj=1:N                                        % simulation of trajectories
+    X = x0;                                                             % set initial state
+    W = 1;                                                              % product in Eq. (26)
+    U = s(randsrc(1,1,[(1:length(s));Ps']));                            % set initial exogenous input
+    for k=2:kh-kp+1
+        X = state_trans(X,U);                                           % sample x_{k+1} ~ p(x_{k+1}|x_k,u_k)
+        U = s(randsrc(1,1,[(1:length(s));Ps']));                        % sample u_{k+1} ~ p(u_{k+1})
+        Expected_Voc = PEv(Voc(X),s(1))*Ps(1) + PEv(Voc(X),s(2))*Ps(2); % Eq. (39)
+        histogram_bins(k) = histogram_bins(k) + Expected_Voc*W;         % sum contribution to histogram
+        W = W*(1-PEv(Voc(X),U));                                        % update product in Eq. (26)
+        if W < rho_min                                                  % if the product is negligible
+            break;                                                      % stop simulating this trajectory
+        end 
+    end
+end
+computation_time = cputime - start_time;                % Eq. (34): computation time
+if sigma_eta == 0.9                                     % load ground truth probability distribution
+    load('ground_truth_eta09.mat');	
+elseif sigma_eta == 0.1
+    load('ground_truth_eta01.mat');
+end
+EoD_time_PMF = histogram_bins./sum(histogram_bins);     % Eq. (22): EoD time probability distribution
+l1_distance = norm(EoD_ground_truth-EoD_time_PMF,1);    % Eq. (33): l1-distance to ground truth
+
+% plot results
+figure
+hold on
+h1 = area(kp:kh,EoD_ground_truth);
+h1.FaceColor = [0.6350, 0.0780, 0.1840];
+h1.FaceAlpha = 0.4;
+h1.EdgeColor = [0.6350, 0.0780, 0.1840];
+h2 = area(kp:kh,EoD_time_PMF);
+h2.FaceColor = [0, 0.5, 0];
+h2.FaceAlpha = 0.4;
+h2.EdgeColor = [0, 0.5, 0];
+xlabel('Time (s)')
+ylabel('Probability')
+legend('Ground truth',sprintf('Method 2: N=%d',N),'location','northwest')
+if sigma_eta == 0.9
+    axis([800 2200 0 3e-3])
+elseif sigma_eta == 0.1
+    axis([3000 3350 0 0.015])
+end
+fprintf('l1-distance (efficacy): %.4f\n',l1_distance)                       % display efficacy
+fprintf('Computation time (efficiency): %.4f seconds\n',computation_time)   % display efficiency
